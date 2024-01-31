0c8a373872d7c4d9f8fd64aa05272d8889ab1cc5
Evgeny <florans1807@mail.ru>
2024-01-31 19:26:35 +0300

    Initial Commit

---
+++ HW4/HW4App.swift
@@ -0,0 +1,17 @@
+//
+//  HW4App.swift
+//  HW4
+//
+//  Created by Флоранс on 31.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct HW4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
